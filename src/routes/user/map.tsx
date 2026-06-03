a7c50c9944513c0e48a40963b933f699f9d7c99d
XiaoLFeng <gm@x-lf.cn>
2026-06-03 23:04:18 +0800

    chore(config): 添加生产环境服务端口号 18586

--- src/routes/user/map.tsx
+++ src/routes/user/map.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute('/user/map')({
 function MapPage() {
   return (
     <iframe
-      src="https://game-map.frontleaves.com/?world=world"
+      src="https://game-map.frontleaves.com:18586/?world=world"
       className="-m-3.5 sm:-m-5 lg:-m-6 h-[calc(100svh-3.25rem)] sm:h-[calc(100svh-3.75rem)] lg:h-[calc(100svh-5.25rem)] w-[calc(100%+1.75rem)] sm:w-[calc(100%+2.5rem)] lg:w-[calc(100%+3rem)] border-0 rounded-xl"
       title="服务器地图"
       allowFullScreen
